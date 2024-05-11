b09c0f5697a10c34e4ea1e4d6868bd61e91e639c
hoang.nguyen <hoang.nguyen@platform.inc>
2024-05-11 11:25:04 +0700

    update react native

--- android/src/main/java/com/kuhulin/RecordCallModule.java
+++ android/src/main/java/com/kuhulin/RecordCallModule.java
@@ -7,9 +7,10 @@ import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.provider.CallLog;
-import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
+
+import javax.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import android.util.Log;
 
 import com.facebook.react.bridge.Arguments;
